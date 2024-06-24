ce7183d552168904f24806e643239ba0d2092d30
Kartik <kartiksaini2410@gmail.com>
2024-06-25 00:26:07 +0530

    change mongo uri

--- backend/db.js
+++ backend/db.js
@@ -1,24 +1,21 @@
 const mongoose = require('mongoose');
 
-const mongoURI = "mongodb+srv://goFood:goFood@cluster0.hitk7lq.mongodb.net/goFood"
+const mongoURI = "mongodb+srv://goFood:goFood@2410@cluster0.hitk7lq.mongodb.net/goFood?retryWrites=true&w=majority&appName=Cluster0";
 
 const connectToMongoDB = async () => {
     try {
         await mongoose.connect(mongoURI, {
-            auth: {
-                username: 'goFood',
-                password: 'goFood@2410'
-            }
+            useNewUrlParser: true,
+            useUnifiedTopology: true
         });
 
-        const fetched_data = await mongoose.connection.db.collection("food_items");
-        const data = await fetched_data.find({}).toArray();
-        const foodCategory = await mongoose.connection.db.collection("food_category");
-        const foodCategories = await foodCategory.find({}).toArray();
-            global.food_items = data;
-            global.foodCategory = foodCategories;
-            console.log("Connected to MongoDB");
-        // console.log(data, "data");
+        const fetched_data = await mongoose.connection.db.collection("food_items").find({}).toArray();
+        const foodCategories = await mongoose.connection.db.collection("food_category").find({}).toArray();
+
+        global.food_items = fetched_data;
+        global.foodCategory = foodCategories;
+        
+        console.log("Connected to MongoDB");
     } catch (error) {
         console.error("Error connecting to MongoDB:", error.message);
     }
