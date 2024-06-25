f6e0eba1026a3cbc20a27d0d2645322dd63ddf4e
Kartik <kartiksaini2410@gmail.com>
2024-06-25 14:53:35 +0530

    port change

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const mongoDB = require('./db');
 
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 5000; 
 
 // Connect to MongoDB
 mongoDB();
@@ -38,6 +38,6 @@ app.use((err, req, res, next) => {
 });
 
 // Start the server
-// app.listen(port, () => {
-//   console.log(`Server is running on port ${port}`);
-// });
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
